20faa83c437c9046fba868aabca145b99869e871
caius-huang <jingranhuang24@gmail.com>
2026-04-19 15:39:20 +0800

    Create narcissus.c

---
+++ narcissus.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d",&d);
+    a=d/100;
+    b=d%100/10;
+    c=d%10;
+    if(d== a*a*a+b*b*b+c*c*c){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+
+}
